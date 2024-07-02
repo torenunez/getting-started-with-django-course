228a0c3c069a0db5689d42111ec7958ee3bb2347
Salvador Nunez <salvador.nunez@salvador.nunez@berkeley.edu>
2024-07-02 19:35:08 +0200

    cleaning up structure

--- Alexandria/home/urls.py
+++
@@ -1,7 +0,0 @@
-# Alexandria/home/urls.py
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('sample/', views.sample),
-]
--- Alexandria/primer/views.py
+++
@@ -1,9 +0,0 @@
-# Alexandria/primer/views.py
-
-from django.http import HttpResponse
-
-
-def say_hello(request):
-    message = 'Hola wey! No se si esta funcionando esto.'
-
-    return HttpResponse(message, content_type='text/plain')
